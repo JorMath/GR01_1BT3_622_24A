1e0a826f2e0884f615a8c68315f5aa823d623932
Cristian-Zambrano <cristian.zambrano@epn.edu.ec>
2024-05-13 22:17:57 -0500

    Create_CompraTest: he creado la prueba unitaria para la clase Compra.

--- src/test/java/ec/edu/epn/logica/CompraTest.java
+++ src/test/java/ec/edu/epn/logica/CompraTest.java
@@ -1,7 +1,31 @@
 package ec.edu.epn.logica;
 
+import org.junit.Test;
+import org.junit.jupiter.api.condition.EnabledOnJre;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-class CompraTest {
+public class CompraTest {
+    Compra compra;
+    Videojuego videojuego;
+    Videojuego videojuegoDTO;
+
+    @Test
+    public void given_un_compra_when_cliente_compra_then_aumenta_iva(){
+        System.out.println("Testando la compra con Iva");
+        compra = new Compra();
+        videojuego = new Videojuego();
+        videojuego.setTitulo("GTA V");
+        videojuego.setPrecio(10);
+        compra.setVideojuego(videojuego);
+        assertEquals(11.5, compra.calcularCompraConIva());
+    }
+
+
+    public void given_un_juego_when_cliente_busca_then_aumenta_iva(){
+
+    }
+
+
 
 }
