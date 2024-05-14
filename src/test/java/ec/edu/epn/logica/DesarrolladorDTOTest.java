fb3fc6e8c120f1209e50edb749b18d0c5469e3a7
jeanpcot <jean.cotera@gmail.com>
2024-05-13 21:39:44 -0500

    Add_Test: Creación de la prueba DesarrolladorDTO para observar si se registra un desarrollador se da de alta en la base de datos

---
+++ src/test/java/ec/edu/epn/logica/DesarrolladorDTOTest.java
@@ -0,0 +1,29 @@
+package ec.edu.epn.logica;
+
+import ec.edu.epn.persistencia.DesarrolladorDTO;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+
+public class DesarrolladorDTOTest {
+    UsuarioDAO usuarioDAO = new UsuarioDAO();
+
+    @Test
+    public void given_datos_when_usuario_registra_then_desarrollador_dado_de_alta(){
+        //Prueba para observar si registra en la base de datos
+        System.out.println("Prueba1");
+        Desarrollador desarrollador = new Desarrollador();
+        desarrollador.setNombre("Jorge");
+        desarrollador.setApellido("Zambrano");
+        desarrollador.setClave("1234");
+        desarrollador.setVideojuegos(new ArrayList<>());
+        usuarioDAO.crearDesarrollador(desarrollador);
+        //desarrolladorDTO.create(desarrollador);
+
+        assertTrue(usuarioDAO.existeDesarrollador("Jorge", "Zambrano", "1234"));
+    }
+}
